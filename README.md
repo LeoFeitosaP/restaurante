1d7e006bd9215b4b2c194a08d09480bf71bba283
LeoFeitosaP <leonardoperes342007@gmail.com>
2025-08-27 18:47:28 -0300

    Adicionando os primeiros arquivos

---
+++ README.md
@@ -0,0 +1,4 @@
+# Projeto para um restaurante
+
+### Backend para o projeto da criação de um sistema para um restaurante
+#### projeto integrador 2025/02
---
+++ src/main/java/com/restaurante/Main.java
@@ -0,0 +1,16 @@
+package com.restaurante;
+
+public class Main {
+
+    /*
+    Funcionalidades a implementar:
+        API Restful para gestâo de produtos, pedidos e clientes
+        Sistema para autenticação de clientes e administradores
+        painel administrativo
+        integração com pagamentos via PIX, cartão e etc
+        notificação por email e wpp
+        db SQL para gestão de pedidos, clientes e histórico
+        relatórios basicos de vendas e desempenho
+
+     */
+}
